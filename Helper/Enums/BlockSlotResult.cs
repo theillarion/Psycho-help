5b344e178cba1194995a77e4c03171b78d41fdf2
Illarion <pro100illarion@gmail.com>
2023-04-21 16:53:32 +0300

    Added new enums

---
+++ Helper/Enums/BlockSlotResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xk7.Helper.Enums
+{
+    public enum BlockSlotResult
+    {
+        Success,
+        UserNotExists,
+        SlotNotExists,
+        Unknown,
+    }
+}
