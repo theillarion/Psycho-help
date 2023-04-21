4d5828c2d25655286b025e65e900acb4eb6b31f9
Illarion <pro100illarion@gmail.com>
2023-04-21 16:59:25 +0300

    Added new methods declaration

--- Services/IDbAsyncService.cs
+++ Services/IDbAsyncService.cs
@@ -18,5 +18,9 @@ namespace Xk7.Services
         Task<DataRow?> GetDataUserByLoginAsync(string login);
         Task<AddLoggingResult> AddLog(string login, LoggingType loggingType);
         Task<DataTable?> GetTable(string nameTable);
+        Task<UserRole> GetUserRoleAsync(string login);
+        Task<bool> IsBusySlotAsync(uint idSlot);
+        Task<AddSlotResult> AddSlotAsync(string employeeLogin, DateOnly slotDate, TimeOnly slotTime);
+        Task<BlockSlotResult> BlockSlotAsync(uint idSlot, string userLogin);
     }
 }
