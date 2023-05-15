9a53743f8a257cac08e9ff1ac0b15a6542c40260
anar- <anar-@LAPTOP-38PTIDOT>
2023-05-15 03:47:48 +0300

    Include notification

---
+++ Model/Notification.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xk7.Model
+{
+    public class Notification : INotifyPropertyChanged
+    {
+        private DateTime dateTimeCreated;
+        public DateTime DateTimeCreated
+        {
+            get { return dateTimeCreated; }
+            set
+            {
+                dateTimeCreated = value;
+                OnPropertyChanged("DateTimeCreated");
+            }
+        }
+        private string description;
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+        private bool isChecked;
+        public bool IsChecked
+        {
+            get { return isChecked; }
+            set
+            {
+                isChecked = value;
+                OnPropertyChanged("IsChecked");
+            }
+        }
+        public Notification()
+        {
+
+        }
+        public Notification(string description, DateTime dateTimeCreated, bool isChecked)
+        {
+            this.description = description;
+            this.dateTimeCreated = dateTimeCreated;
+            this.isChecked = isChecked;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
